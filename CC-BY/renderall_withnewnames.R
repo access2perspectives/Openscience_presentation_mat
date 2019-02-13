64c253e6f56905d0c8a8788db5aee33dd2bb5066
colombj <julien.colomb@fu-berlin.de>
2019-02-13 15:26:59 +0100

    put design back, small differences

--- CC-BY/renderall_withnewnames.R
+++ CC-BY/renderall_withnewnames.R
@@ -1,4 +1,4 @@
-setwd("non_website")
+#setwd("non_website")
 
 
 rmarkdown::render("presentation_d1.Rmd",output_file = "01_openscience.html")
@@ -8,3 +8,4 @@ rmarkdown::render("presentation_d2.Rmd",output_file = "13_reproducibility.html")
 rmarkdown::render("reproducibleanalysis_intro.Rmd",output_file = "15_repr_analysis.html")
 rmarkdown::render("OA_details.Rmd",output_file = "11_OA.html")
 rmarkdown::render("Matmeth.Rmd",output_file = "14_material_method.html")
+rmarkdown::render("gitlab_in_practice.Rmd",output_file = "20_Use_gitlab.html")
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ open science, open data, open access, RDM open methodology, gitlab, reproducibil
 
 if the material is not CC-BY, ask permission for reuse.
 
-You can use the R code to transform Rmd files (ios_presentation) into html5 presentation (giving them new names).
+You can use the R code to transform Rmd files (ios_presentation) into html5 presentation (giving them new names). It use the .yaml and css files to set the presentation options and design.
+
 keynote files without license were not exported to pdf files.
